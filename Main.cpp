56be71ac016cd8c8c50ffacfa051d8a4c4f7eafe
Seif Hany <seif.naguib@gmail.com>
2025-01-03 18:59:48 +0200

    Fibonacci and Factorial

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 
-void printInfo(std::string &name);
+typedef int (*FunctionType)(const int);
 
 int fibonacci(const int n);
-int main() {
-  int n = 0;
+int factorial(const int n);
 
+void invoke(FunctionType func, std::string name);
+
+int main() {
+  invoke(fibonacci, "fibonacci");
+  // invoke(factorial, "factorial");
+}
+
+void invoke(FunctionType func, std::string name) {
+  int n = 0;
   while (n >= 0) {
     std::cout << "fibonacci(" << n << "): ";
-    std::cout << fibonacci(n) << "\n";
+    std::cout << func(n) << "\n";
     std::cout << "\nEnter number: ";
     std::cin >> n;
   }
@@ -20,4 +28,10 @@ int fibonacci(const int n) {
   if (n == 1)
     return 1;
   return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int factorial(const int n) {
+  if (n == 0)
+    return 1;
+  return n * factorial(n - 1);
 }
